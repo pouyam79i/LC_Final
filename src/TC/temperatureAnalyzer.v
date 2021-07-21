01b43e431b37a30368e08001e8c1749e4f436371
ArsalanAa <58984008+ArsalanAa@users.noreply.github.com>
2021-07-21 16:35:58 +0430

    Update and rename 2.v to temperatureAnalyzer.v

--- src/TC/temperatureAnalyzer.v
+++ src/TC/temperatureAnalyzer.v
@@ -6,22 +6,31 @@
 --  *******************************************************
 --  All Rights reserved (C) 2020-2021
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9829039   - 9831007 - 9831059
+--  Student Name: Pouya     - Mehran  - Abolfazl
+--  Last Name:    Mohammadi - Aksari  - Moradi Feijani
 --  *******************************************************
 --  Additional Comments:
---
+--  	using this module in FD module! -> used to build 8-bit comparator
 --*/
+
 
 /*-----------------------------------------------------------
 ---  Module Name: temperatureAnalyzer 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
+
 module temperatureAnalyzer(
- temperature,
- temperatureAbnormality);
-input [7:0] temperature;
-output temperatureAbnormality;
- assign temperatureAbnormality = (temperature > 39) || (temperature < 35);
+	temperature,
+	lowTempAbnormality,
+	highTempAbnormality
+    );
+	
+	input [7:0] temperature;
+	output lowTempAbnormality;
+	output highTempAbnormality;
+	
+	assign lowTempAbnormality  = (temperature < 8'b00100011);
+	assign highTempAbnormality = (temperature > 8'b00100111);
+	
 endmodule
