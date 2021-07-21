11f63797141a89ed026e1515076f719e6ec4dd4a
ArsalanAa <58984008+ArsalanAa@users.noreply.github.com>
2021-07-21 15:55:50 +0430

    Update and rename 3.v to pressureAbnormalityDetector.v

--- src/pressure/pressureAbnormalityDetector.v
+++ src/pressure/pressureAbnormalityDetector.v
@@ -6,22 +6,29 @@
 --  *******************************************************
 --  All Rights reserved (C) 2020-2021
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9829039   - 9831007 - 9831059
+--  Student Name: Pouya     - Mehran  - Abolfazl
+--  Last Name:    Mohammadi - Aksari  - Moradi Feijani
 --  *******************************************************
 --  Additional Comments:
---
+--  	
 --*/
 
 /*-----------------------------------------------------------
 ---  Module Name: pressureAbnormalityDetector 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
-module pressureAbnormalityDetector(
- pressureData,
- presureAbnormality);
-input [5:0] pressureData;
-output presureAbnormality;
- // write your code here, please.
+
+module pressureAbnormalityDetector( 
+	input [5:0] pressureData,
+	output pressureAbnormality
+	);
+	
+	wire ParityErrorCheckerOutput,PressureAnalyzerOutput;
+	
+	ParityErrorChecker pec(pressureData,ParityErrorCheckerOutput);
+	PressureAnalyzer pa(pressureData[4:0],PressureAnalyzerOutput);
+	
+	assign pressureAbnormality = ParityErrorCheckerOutput & PressureAnalyzerOutput;
+
 endmodule
