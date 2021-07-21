017240530d885f8f618ef382e5591f8f394fc59a
ArsalanAa <58984008+ArsalanAa@users.noreply.github.com>
2021-07-21 15:56:51 +0430

    Update and rename 2.v to pressureAnalyzer.v

--- src/pressure/pressureAnalyzer.v
+++ src/pressure/pressureAnalyzer.v
@@ -6,22 +6,23 @@
 --  *******************************************************
 --  All Rights reserved (C) 2020-2021
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9829039   - 9831007 - 9831059
+--  Student Name: Pouya     - Mehran  - Abolfazl
+--  Last Name:    Mohammadi - Aksari  - Moradi Feijani
 --  *******************************************************
 --  Additional Comments:
---
+--  	
 --*/
 
 /*-----------------------------------------------------------
 ---  Module Name: pressureAnalyzer 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
-module pressureAnalyzer(
- pData,
- pWarning);
-input [4:0] pData;
-output pDarning;
- // write your code here, please.
+	
+module PressureAnalyzer(
+	input [4:0] pData,
+	output pWarning
+	);
+	assign pWarning = ~( (~pData[4] & pData[3]) | (pData[4] & ~pData[3] & ~pData[2]) | (pData[4] & ~pData[3] & ~pData[1]) | (pData[4] & ~pData[3] & ~pData[0]));
 endmodule
+	
