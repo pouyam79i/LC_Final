d2ef3bd7eb0392b7879868a5b4eb4d628a44c14e
ArsalanAa <58984008+ArsalanAa@users.noreply.github.com>
2021-07-21 16:36:56 +0430

    Update and rename 3.v to temperatureAbnormalityDetector.v

--- src/TC/temperatureAbnormalityDetector.v
+++ src/TC/temperatureAbnormalityDetector.v
@@ -6,26 +6,34 @@
 --  *******************************************************
 --  All Rights reserved (C) 2020-2021
 --  *******************************************************
---  Student ID  : 
---  Student Name: 
---  Student Mail: 
+--  Student ID  : 9829039   - 9831007 - 9831059
+--  Student Name: Pouya     - Mehran  - Abolfazl
+--  Last Name:    Mohammadi - Aksari  - Moradi Feijani
 --  *******************************************************
 --  Additional Comments:
---
+--  	using this module in FD module! -> used to build 8-bit comparator
 --*/
 
 /*-----------------------------------------------------------
 ---  Module Name: temperatureAbnormalityDetector 
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
+
 module temperatureAbnormalityDetector(
- factotyBaseTemp,
- factotyTempCoef,
- tempSensorValue,
- temperatureAbnormality);
-input [4:0] factotyBaseTemp;
-input [3:0] factotyTempCoef;
-input [3:0] tempSensorValue;
-output temperatureAbnormality;
- // write your code here, please.
+	factotyBaseTemp,
+	factotyTempCoef,
+	tempSensorValue,
+	lowTempAbnormality,
+	highTempAbnormality);
+	
+	input [4:0] factotyBaseTemp;
+	input [3:0] factotyTempCoef;
+	input [3:0] tempSensorValue;
+	output lowTempAbnormality;
+	output highTempAbnormality;
+	
+	wire [7:0] temperature;
+	temperatureCalculator tc(factotyBaseTemp, tempSensorValue, factotyTempCoef, temperature);
+	temperatureAnalyzer ta(temperature, lowTempAbnormality, highTempAbnormality);
+	
 endmodule
